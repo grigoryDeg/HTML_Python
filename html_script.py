29bb897310f7ab0ed8fc09a7dcea572afd06e19e
grigoryDeg <degtyareff.gri@mail.ru>
2018-02-16 22:12:08 +0300

    adding

---
+++ html_script.py
@@ -0,0 +1,26 @@
+
+print("<HTML>")
+colorBG =("#808000")
+color1 = ("#006400")
+color2 = ("black")
+location1 = ("center")
+location2 = ("left")
+font1 = ("verdana")
+font2 =("arial")
+fontsize = 38
+fontsize2 = 30
+border1 = 5
+width1 = 400
+height1 = 350
+print('<body style=background-color:', colorBG, ';>', sep='')
+print('<div align=', location1, '>', sep='')
+print('<h1 style=font-family:',font1,';','color:',color1 , ';font-size:', fontsize,'px',';>Гитара </h1> </div>', sep='')
+print('<div align=',location2,'><img src= "http://evrikak.ru/wp-content/uploads/2014/03/Ua5rWt6.jpeg" ', 'border=', border1, ' align=', location2,' width=', width1,' height=', height1, ' </div>', sep='')
+print('<p style="font-family:', font2,';color:', color2,';font-size:', fontsize2,'px;">', sep='')
+print("Гитара — струнный щипковый музыкальный инструмент, один из самых распространённых в мире. Применяется в качестве аккомпанирующего инструмента во многих музыкальных стилях, а также как сольный классический инструмент.</p>")
+print('<p style="font-family:', font2,';color:', color2,';font-size:', fontsize2,'px;">', sep='')
+print('Является основным инструментом в таких стилях музыки, как блюз, кантри, фламенко, рок-музыка и многих формах популярной музыки. Изобретённая в XX веке электрическая гитара оказала сильное воздействие на массовую культуру.</p>')
+print('<p style="font-family:', font2,';color:', color2,';font-size:', fontsize2,'px;">', sep='')
+print('Исполнитель на гитаре называется гитарист. Человек, изготовляющий и ремонтирующий гитары, называется гитарный мастер или лютье.</p>')
+print('</body>')
+print('</HTML>')
